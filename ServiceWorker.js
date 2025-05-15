3d29e56552243f1beecd5fbf3e189baf952b59eb
Даниил <jugglerrus@gmail.com>
2025-05-15 21:37:15 +0300

    6

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-DoodleJumpTest-1.0";
+const contentToCache = [
+    "Build/36db41ea7bc6b6776bbca25cc146a9d4.loader.js",
+    "Build/198702bcd00883dfc80eab4cf06e69bf.framework.js",
+    "Build/f944fe980d4f25eb603d064da43a0780.data",
+    "Build/fbf770ce46f3e7415dbdc4b39aeaeb88.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
